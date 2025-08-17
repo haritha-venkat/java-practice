5df7fe6c91a92f9fc50f6c7ff656aed907e13f44
haritha-venkat <harithashree2125@gmail.com>
2025-08-17 13:14:26 +0530

    java size program

---
+++ day14.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class day14 {
+    public static void main(String[] args)
+    {
+        System.out.println("enter size");
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+
+        switch(size) {
+            case 29:
+               System.out.println("small");
+               break;
+            case 30:
+               System.out.println("medium");
+               break;
+            case 38:
+               System.out.println("large");
+               break;
+            case 42:
+               System.out.println("Xlarge");
+               break;
+
+            default:
+               System.out.println("invalid input ");
+               break;
+
+        }
+        scan.close();
+
+    }
+}
